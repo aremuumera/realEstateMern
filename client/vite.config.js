530358a3dd8f768031787fc48eac2922fa3bffc0
Aremu <aremu.script@gmail.com>
2024-07-14 18:12:36 +0100

    yikes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase this limit if necessary
+  },
+});
