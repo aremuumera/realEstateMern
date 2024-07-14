699c0b8a089868c37aaa8c02356369ef695ddf74
Aremu <aremu.script@gmail.com>
2024-07-14 20:48:25 +0100

    tikes

--- api/app.js
+++ api/app.js
@@ -11,14 +11,21 @@ import cors from 'cors'
 
 const app = express();
 
-app.use(cors({
-    // origin:"http://localhost:5174",
-    origin: ["https://realestatemern-1-qgpr.onrender.com", "https://real-estate-three-orcin.vercel.app"],
-    credentials:true,            
-    optionSuccessStatus:200,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"]
-}));
+const allowedOrigins = [
+    'https://real-estate-three-orcin.vercel.app',
+    'https://realestatemern-1-qgpr.onrender.com'
+  ];
+  app.use(cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }));
 app.use(express.json());
 app.use(cookieParser());
 
