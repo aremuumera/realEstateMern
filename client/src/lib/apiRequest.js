67593232234c5c562fe0450c25e05a052c17a530
Aremu <aremu.script@gmail.com>
2024-07-15 02:45:05 +0100

    yikes

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://realestatemern-f43d.onrender.com",
+  baseURL: "https://realestatemern-f43d.onrender.com/api",
   // baseURL: "http://localhost:8000",
 
   withCredentials: true,
